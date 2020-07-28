21fd7a886c55aa5e1e389a5c5a1b71e63b03b233
Narendra Nelluri <Narendra_Nelluri@outlook.com>
2020-07-28 13:40:47 +0530

    created services

---
+++ Src/ContributorService/SecoandService.Core/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace SecoandService.Core
+{
+    public class Class1
+    {
+    }
+}
---
+++ Src/PackageService/FirstService.Core/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace FirstService.Core
+{
+    public class Class1
+    {
+    }
+}
